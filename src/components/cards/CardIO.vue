9837bb1edcd4c66debe0703f90fafee6138d58f3
Caique Bispo <caiquebispo0023@gmail.com>
2023-05-25 17:20:06 -0300

    Creating CardIO component, and adding panel sessions

--- src/components/cards/CardIO.vue
+++ src/components/cards/CardIO.vue
@@ -1,11 +1,32 @@
 <template>
     <div class="card mb-10">
         <!--begin::Header-->
-        <div class="card-header card-header-stretch">
-
+        <div class="card-header card-header-stretch d-flex align-items-center">
+            <div class="header d-flex">
+                <div class="header-icon">
+                    <slot name="header-icon"></slot>
+                </div>
+                <div class="header-title ms-3">
+                    <slot name="header-title"></slot>
+                </div>
+            </div>
         </div>
         <div class="card-body">
-            <slot name="card-content-bod"></slot>
+            <slot name="card-content-body"></slot>
         </div>
     </div>
 </template>
+<style>
+    .header-icon i{
+        font-size: 1.4em !important;
+        color: #00FF7F !important;
+    }
+    .header-title{
+        display: table-cell;
+        vertical-align: middle;
+        font-size: 15px;
+        font-weight: 600;
+        font-family: 'Poppins';
+        color: #00365D;
+    }
+</style>
--- src/views/manager-panel/Dashboard.vue
+++ src/views/manager-panel/Dashboard.vue
@@ -2,6 +2,7 @@
     import Card from "@/components/cards/CardIO.vue"
 </script>
 <template>
+    <!-- Indicador de receita -->
     <Card>
         <template v-slot:header-icon>
             <i class="fa fa-dollar"></i>
@@ -9,7 +10,117 @@
         <template v-slot:header-title>RECEITA TOTAL</template>
 
         <template v-slot:card-content-body>
-            is here
+           ....
         </template>
     </Card>
+    <!-- end Indicador de receita -->
+    <!-- Indicador de receita  das diárias-->
+    <Card>
+        <template v-slot:header-icon>
+            <i class="fa fa-dollar"></i>
+        </template>
+        <template v-slot:header-title>RECEITA DE DIÁRIAS</template>
+
+        <template v-slot:card-content-body>
+           ....
+        </template>
+    </Card>
+    <!-- end Indicador de receita das diárias -->
+    <!-- Indicador de diária média-->
+    <Card>
+        <template v-slot:header-icon>
+            <i class="fa fa-dollar"></i>
+        </template>
+        <template v-slot:header-title>DIÁRIA MÉDIA</template>
+
+        <template v-slot:card-content-body>
+           ....
+        </template>
+    </Card>
+    <!-- end Indicador de diária média -->
+    <!-- Indicador de diária média-->
+    <Card>
+        <template v-slot:header-icon>
+            <i class="fa fa-dollar"></i>
+        </template>
+        <template v-slot:header-title>OCUPAÇÃO</template>
+
+        <template v-slot:card-content-body>
+           ....
+        </template>
+    </Card>
+    <!-- end Indicador de diária média -->
+    <!-- Indicador de room night-->
+    <Card>
+        <template v-slot:header-icon>
+            <i class="fa fa-dollar"></i>
+        </template>
+        <template v-slot:header-title>ROOM NIGHTS</template>
+
+        <template v-slot:card-content-body>
+           ....
+        </template>
+    </Card>
+    <!-- end Indicador de room night -->
+    <!-- Indicador de revpar-->
+    <Card>
+        <template v-slot:header-icon>
+            <i class="fa fa-dollar"></i>
+        </template>
+        <template v-slot:header-title>Rev Par</template>
+
+        <template v-slot:card-content-body>
+           ....
+        </template>
+    </Card>
+    <!-- end Indicador de revpar -->
+    <!-- Indicador de comparativo de indicadores-->
+    <Card>
+        <template v-slot:header-icon>
+            <i class="fa fa-dollar"></i>
+        </template>
+        <template v-slot:header-title>COMPARATIVO DE INDICADORES</template>
+
+        <template v-slot:card-content-body>
+           ....
+        </template>
+    </Card>
+    <!-- end Indicador de comparativo de indicadores -->
+    <!-- Indicador de ranking de clientes-->
+    <Card>
+        <template v-slot:header-icon>
+            <i class="fa fa-dollar"></i>
+        </template>
+        <template v-slot:header-title>RANKING DE CLIENTES</template>
+
+        <template v-slot:card-content-body>
+           ....
+        </template>
+    </Card>
+    <!-- end Indicador de ranking de clientes -->
+    <!-- Indicador de analise de segmento-->
+    <Card>
+        <template v-slot:header-icon>
+            <i class="fa fa-dollar"></i>
+        </template>
+        <template v-slot:header-title>ANÁLISE DE SEGMENTO</template>
+
+        <template v-slot:card-content-body>
+           ....
+        </template>
+    </Card>
+    <!-- end Indicador de analise de segmento -->
+    <!-- Indicador de analise de segmento-->
+    <Card>
+        <template v-slot:header-icon>
+            <i class="fa fa-dollar"></i>
+        </template>
+        <template v-slot:header-title>RESUMO DA PICKUP</template>
+
+        <template v-slot:card-content-body>
+           ....
+        </template>
+    </Card>
+    <!-- end Indicador de analise de segmento -->
+     
 </template>
